833932364ff1640980a76f6a41b6c0ebe306ef9d
Rajdeep Mukherjee <rajdeepmukhrj@gmail.com>
2017-03-05 22:18:06 +0530

    added classifier programs

--- numpyMatplotlibPandas.py
+++ numpyMatplotlibPandas.py
@@ -1,104 +1,106 @@
-import numpy as np
-heights=[1.5,1.7,1.6,1.2,1.2]
-heights_array=np.array(heights)
-print(heights_array)
-"""
-Now there are various interesting calculations that we can do with numpy arrays
-Firstly if we do 
-
-npArray + npArray the result is a element wise addition of all the elements of the array
-Similarly we can do a divide between 2 numpy arrays and that will also result in an element wise division of the each array
-One important thing to note is that numpy arrays cannot store data of different types. For example if we have an int  a float and a string all will be converted to a string.
-
-Accessing elements of a numpy array: array[<index>]
-
-Another interesting feature:
-Lets say we want an list of all the elements in the numpy array that is greater than a specific number. In that case we can simply use the greater than sign in the following way:
-
-numpyArray>x
-The result is an array of booleans that says whether a particular element is greater than x
-"""
-val=heights_array[heights_array>1.5]
-# the above line returns a array of values that are greater than 1.5 from heights_array
-print (val)
-
-"""
-2D Numpy Array
-2d numpy arrays are made in a similar way as normal 2d lists in python
-"""
-np_2d=np.array([[1.7,1.5,1.2,1],[1.2,1.9,2.0,2.1]])
-# so above is a 2d numpy array
-print(np_2d)
-print(np_2d.shape)
-# shape is an attribute of a numpy array that can give us the dimensions of numpy array
-print (np_2d[0])
-# the above line prints the first row
-print(np_2d[0][1])
-# as expected prints the first row , 2nd col 
-# also
-print(np_2d[0,2])
-# similarly
-print(np_2d[:,1:3])# remember in 1:3, 3 is not included, it just means 1 and 2 th element of both rows
-
-"""
-Data Analysis:
-Mean: np.mean(<array>)
-Median: np.median(<array>)
-Correlation Coefficient: np.corrcoef(<array>,<array>)
-Standard deviation: np.std(<array>)
-Other functions are also there like sum and sort, random number generation functions are also there, check documentation for more insight
-"""
-"""
-Data Visulation:
-Library: matplotlib
-inside the matplotlib package there is pyplot sob package and this gives you all basic pyplot functionality
-
-"""
-import matplotlib.pyplot as plt
-year=[2009,2010,2011,2012,2013]
-population=[1.7,1.8,1.4,1.4,1.8]
-plt.plot(year,population)
-# plot methods plots the data in the form of a line chart, the first list corresponds to x axis
-plt.show()
-# plot actually shows the plot to us
-plt.scatter(year,population)
-# the above code generates a scatter plot instead of a line plot 
-plt.show()
-"""
-Data Analysis: concept of Histograms in pyplot
-There is a concept of bins in histograms in pyplot. Firstly seeing the documentation of the method hist we see  that the first 2 arguements are x and bins, x is the list of values to be considered, bins is the number of partitions the list x is divided into, python will automatically figure out how many values are there in each one of those bins
-
-In plotting different values it is important to note that the dimenstion of both x and y must be the same otherwise a ValueError is raised
-"""
-x=[0,0.6,0.5,1.5,1.7,1.7,1.2,1.9,1.8,2.5,2.4]
-plt.hist(x,bins=4)
-plt.show()
-"""
-Customizing your graphs
-"""
-y=[1.5,2.3,1.3,1.8,1.9,2.5,2.5,1.7,2.9,2.5,1.2]
-# the plot method is only for plotting a line graph
-# plt.plot(x,y)
-plt.fill_between(x,y,0,color='green')
-# the above method is used for filling under the line graph, the 3rd arguement is for filling uptill 0
-plt.xlabel('x- axis')
-plt.ylabel('y- axis')
-plt.title('Sample graph')
-# we can change ticks on the y axis, think of ticks simply as the number of steps, the second arguement is the repersentation of # the ticks
-plt.yticks([0,0.25,0.5,0.75,1,1.25,1.5,1.75,2.0,2.25,2.5,2.75,3.0])
-plt.show()
-
-"""
-Pandas Package:
-High level data manipulation tool used by data scientists. We can store data in pandas in data frames, data frames are tables
-Generally data in data frames are not manually entered, rather they are entered from files especially csv files
-"""
-import pandas as pd
-# for reading from a csv file into a data frame we can simply
-# x=pd.read_csv(<path to csv>, index_col=<column number in which the row indices are stored>)
-# Column access: <name of data frame object>.<name of column as a property of the object itself> or <name of data frame object>[<name of the column>]
-# adding a column: <name of the data frame object>[<name of the column>]=[<list of values for each row>]
-# Example datFrame["col1"]=[row0Val,row1Val,row2Val]
-# Pandas is based on numpy and so we can carry out operations like divide one list(or really a numpy array) by annother
-# Accessing a row: <name of the data frame object>.loc[<row name that is the first column>]
-# Accesssing elements: <name of the data frame object>.loc[<row name>,<column name>]
+def main():
+    import numpy as np
+    heights=[1.5,1.7,1.6,1.2,1.2]
+    heights_array=np.array(heights)
+    print(heights_array)
+    """
+    Now there are various interesting calculations that we can do with numpy arrays
+    Firstly if we do 
+    
+    npArray + npArray the result is a element wise addition of all the elements of the array
+    Similarly we can do a divide between 2 numpy arrays and that will also result in an element wise division of the each array
+    One important thing to note is that numpy arrays cannot store data of different types. For example if we have an int  a float and a string all will be converted to a string.
+    
+    Accessing elements of a numpy array: array[<index>]
+    
+    Another interesting feature:
+    Lets say we want an list of all the elements in the numpy array that is greater than a specific number. In that case we can simply use the greater than sign in the following way:
+    
+    numpyArray>x
+    The result is an array of booleans that says whether a particular element is greater than x
+    """
+    val=heights_array[heights_array>1.5]
+    # the above line returns a array of values that are greater than 1.5 from heights_array
+    print (val)
+    
+    """
+    2D Numpy Array
+    2d numpy arrays are made in a similar way as normal 2d lists in python
+    """
+    np_2d=np.array([[1.7,1.5,1.2,1],[1.2,1.9,2.0,2.1]])
+    # so above is a 2d numpy array
+    print(np_2d)
+    print(np_2d.shape)
+    # shape is an attribute of a numpy array that can give us the dimensions of numpy array
+    print (np_2d[0])
+    # the above line prints the first row
+    print(np_2d[0][1])
+    # as expected prints the first row , 2nd col 
+    # also
+    print(np_2d[0,2])
+    # similarly
+    print(np_2d[:,1:3])# remember in 1:3, 3 is not included, it just means 1 and 2 th element of both rows
+    
+    """
+    Data Analysis:
+    Mean: np.mean(<array>)
+    Median: np.median(<array>)
+    Correlation Coefficient: np.corrcoef(<array>,<array>)
+    Standard deviation: np.std(<array>)
+    Other functions are also there like sum and sort, random number generation functions are also there, check documentation for more insight
+    """
+    """
+    Data Visulation:
+    Library: matplotlib
+    inside the matplotlib package there is pyplot sob package and this gives you all basic pyplot functionality
+    
+    """
+    import matplotlib.pyplot as plt
+    year=[2009,2010,2011,2012,2013]
+    population=[1.7,1.8,1.4,1.4,1.8]
+    plt.plot(year,population)
+    # plot methods plots the data in the form of a line chart, the first list corresponds to x axis
+    plt.show()
+    # plot actually shows the plot to us
+    plt.scatter(year,population)
+    # the above code generates a scatter plot instead of a line plot 
+    plt.show()
+    """
+    Data Analysis: concept of Histograms in pyplot
+    There is a concept of bins in histograms in pyplot. Firstly seeing the documentation of the method hist we see  that the first 2 arguements are x and bins, x is the list of values to be considered, bins is the number of partitions the list x is divided into, python will automatically figure out how many values are there in each one of those bins
+    
+    In plotting different values it is important to note that the dimenstion of both x and y must be the same otherwise a ValueError is raised
+    """
+    x=[0,0.6,0.5,1.5,1.7,1.7,1.2,1.9,1.8,2.5,2.4]
+    plt.hist(x,bins=4)
+    plt.show()
+    """
+    Customizing your graphs
+    """
+    y=[1.5,2.3,1.3,1.8,1.9,2.5,2.5,1.7,2.9,2.5,1.2]
+    # the plot method is only for plotting a line graph
+    # plt.plot(x,y)
+    plt.fill_between(x,y,0,color='green')
+    # the above method is used for filling under the line graph, the 3rd arguement is for filling uptill 0
+    plt.xlabel('x- axis')
+    plt.ylabel('y- axis')
+    plt.title('Sample graph')
+    # we can change ticks on the y axis, think of ticks simply as the number of steps, the second arguement is the repersentation of # the ticks
+    plt.yticks([0,0.25,0.5,0.75,1,1.25,1.5,1.75,2.0,2.25,2.5,2.75,3.0])
+    plt.show()
+    
+    """
+    Pandas Package:
+    High level data manipulation tool used by data scientists. We can store data in pandas in data frames, data frames are tables
+    Generally data in data frames are not manually entered, rather they are entered from files especially csv files
+    """
+    import pandas as pd
+    # for reading from a csv file into a data frame we can simply
+    # x=pd.read_csv(<path to csv>, index_col=<column number in which the row indices are stored>)
+    # important thing to note in the read_csv function is that the path to csv file should be the absolute path
+    # Column access: <name of data frame object>.<name of column as a property of the object itself> or <name of data frame object>[<name of the column>]
+    # adding a column: <name of the data frame object>[<name of the column>]=[<list of values for each row>]
+    # Example datFrame["col1"]=[row0Val,row1Val,row2Val]
+    # Pandas is based on numpy and so we can carry out operations like divide one list(or really a numpy array) by annother
+    # Accessing a row: <name of the data frame object>.loc[<row name that is the first column>]
+    # Accesssing elements: <name of the data frame object>.loc[<row name>,<column name>]
---
+++ useScikitLearn.py
@@ -0,0 +1,85 @@
+"""
+Now that we know a little bit of numpy, matplotlib and pandas lets try and simulate a few values here first and then work on them
+"""
+"""
+This first function is going to simulate data points for us. First thing to note here is that just by looking at how points are being generated and does not actually give us idea of what we are finally gonna achieve, our final goal is to make a dataframe that will contain x and y as the 2 attributes for each entry and a label Z. Now based on whether the label is 0 or 1 we are making 2 different distributions so that we can get a fair amount of dissimilarity between the points in the graph, they should not be too inter-mingled for the purposes of the simulation.
+"""
+def sim_log_data(meanX1,meanY1,totalPointsN1,sd1,meanX2,meanY2,totalPointsN2,sd2):
+    import pandas as pd
+    import numpy.random as nr
+    wx1=nr.normal(loc=meanX1,scale=sd1,size=totalPointsN1)
+    wy1=nr.normal(loc=meanY1,scale=sd1,size=totalPointsN1)
+    # the above code simulates points (x1,y1) for the labels z1 which we can decide to be either 1 or 0
+    # consult documentation for nr.normal but for a gist the parameters are (mean, sd, total points to be generated). Returns 
+    # numpy ndarray which will store points generated out of a normal distribution
+    z1=[0]*totalPointsN1 
+    # z1 is essentially a list of 0s totalPointsN1 times
+    wx2=nr.normal(loc=meanX2,scale=sd2,size=totalPointsN2)
+    wy2=nr.normal(loc=meanY2,scale=sd2,size=totalPointsN2)
+    z2=[1]*totalPointsN2
+
+    # considering z1 to be all 0s and z2 to be all 1s is completely arbitrary
+
+    # now we build a dataframe of all these points generated
+    df1=pd.DataFrame({'x':wx1,'y':wy1,'z':z1})
+    df2=pd.DataFrame({'x':wx2,'y':wy2,'z':z2})
+
+    # so the DataFrame module accepts a dictionary with the keys as the column headers and values as lists containing records or # rows
+
+    return pd.concat([df1,df2],axis=0,ignore_index=True)
+
+    # pd.concat simply concatenates 2 dataframes here, axis =0 means concatenation takes place one below the other
+
+def plot_class(df):
+    import matplotlib.pyplot as plt
+    fig=plt.figure(figsize=(8,8))
+    # (8,8) is a (w,h) tuple which says that the figure size will be 8 by 8 inches, retuns a Figure instance
+    fig.clf()
+    # clf clears the current figure
+    ax=fig.gca()
+    # create axes
+    df[df.z==1].plot(kind='scatter',x='x',y='y',ax=ax,alpha=1.0,color='Red',marker='x',s=40)
+    df[df.z==0].plot(kind='scatter',x='x',y='y',ax=ax,alpha=1.0,color='DarkBlue',marker='o',s=40)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_title('Classes vs X and Y')
+    return 'Done'
+"""
+On plotting the garph one thing that we can notice is that the random nature 
+of the plot surely means that there is no striaght forward classification of 
+the data, So we will use the logsitic regression model in order to train the 
+machine so that it classify points accordingly
+"""
+
+def logistic_mod(df,logProb=1.0):
+    from sklearn import linear_model
+    """
+    The first step is essentially preparing the data 
+    """
+    # df.shape actually gives the dimensions of the dataframe in the form 
+    # of a tuple, extracting values from a tuple is using square brac notation
+    nrow=df.shape[0]
+    # nrow is the number of rows in the dataframe
+    X=df[['x','y']].as_matrix().reshape(nrow,2)
+    # reshape kinda forces the array to be of shape nrow,2
+    Y=df.z.as_matrix().ravel()
+    # ravel returs a contiguous flattened array
+    """
+    Compute the logistic regression model
+    """
+    lg=linear_model.LogisticRegression()
+    # so we make the logistic regression model and name it lg
+    logr=lg.fit(X,Y)
+    # then we try to fit a curve y=f(x)
+    temp=logr.predict_log_proba(X)
+    # temp is a numpy array
+    df['predicted']=[1 if (logProb >p[1]/p[0]) else 0 for p in temp]
+    return df
+
+           
+def main():
+    df=sim_log_data(-1,-1,10,1,1,1,10,1)
+    plot_class(df)
+
+if __name__=="__main__":
+    main()
